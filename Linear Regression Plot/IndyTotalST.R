3b26076cebf974ad0a5616f50256096d1a16b2e3
Matthew Kwan <matthew.k913@gmail.com>
2018-04-07 03:17:02 -0400

    Added Plots for Industrial Engineers

--- Linear Regression Plot/IndyTotalST.R
+++ Linear Regression Plot/IndyTotalST.R
@@ -11,9 +11,10 @@ library(MASS);
 studres(Fit);
 Fit$fitted.values;
 qqnorm(rstandard(Fit));
-qqlin(rstandard(Fit));
-plot (rstandard(Fit)~Fit$fitted.values);
+qqline(rstandard(Fit));
+plot (rstandard(Fit)~Fit$fitted.values, main='Standarized Residuals vs Fitted Values');
 abline(0,0);
-
-
-
+plot (rstandard(Fit), main = 'standarized residuals vs observation number')
+abline(0,0);
+plot (rstandard(Fit)~Score, main = 'standarized residuals vs score');
+abline(0,0);
