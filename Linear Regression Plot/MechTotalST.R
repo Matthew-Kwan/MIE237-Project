66ca90afa9511f2921773263a4d9932df6955515
Matthew Kwan <matthew.k913@gmail.com>
2018-04-07 03:22:50 -0400

    Added Mechanical Engineer Plots

--- Linear Regression Plot/MechTotalST.R
+++ Linear Regression Plot/MechTotalST.R
@@ -5,5 +5,18 @@ plot(Score, Time, main="Mechanical Engineers: Score vs Time", xlab="Score", ylab
 Fit<-lm(Score~Time);
 summary(Fit);
 abline(Fit);
+Fit$residuals;
+rstandard(Fit);
+library(MASS);
+studres(Fit);
+Fit$fitted.values;
+qqnorm(rstandard(Fit));
+qqline(rstandard(Fit));
+plot (rstandard(Fit)~Fit$fitted.values, main='Standarized Residuals vs Fitted Values');
+abline(0,0);
+plot (rstandard(Fit), main = 'standarized residuals vs observation number')
+abline(0,0);
+plot (rstandard(Fit)~Score, main = 'standarized residuals vs score');
+abline(0,0);
 
 
